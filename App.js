29ad6f8aec10ab612a2c2d04b916519aef633f56
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-11-03 21:09:20 +0545

    tailwindcss added and create some folder

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello gyes ! its restaurant</Text>
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-2xl text-red-600">Hello gyes ! its restaurant</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
