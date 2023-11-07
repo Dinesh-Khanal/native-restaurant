ba5e88707d7b58545c5546cc9402246aed4434b8
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-11-07 22:55:37 +0545

    redux added

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import HomeScreen from "./screens/home-screen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import RestaurantScreen from "./screens/restaurant-screen";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
--- components/Dish-row.js
+++ components/Dish-row.js
@@ -1,9 +1,25 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import * as Icon from "react-native-feather";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItemsById,
+} from "../redux/basket-slice";
 import { themeColors } from "../theme";
 import React from "react";
 
 export default function DishRow({ dish }) {
+  const dispatch = useDispatch();
+  const basketItems = useSelector((state) =>
+    selectBasketItemsById(state, dish.id)
+  );
+  const handleIncrease = () => {
+    dispatch(addToBasket(dish));
+  };
+  const handleDecrease = () => {
+    dispatch(removeFromBasket(dish));
+  };
   return (
     <View className="flex-row items-center bg-white p-3 rounded-3xl shadow-2xl mb-3 mx-2">
       <Image
@@ -22,6 +38,8 @@ export default function DishRow({ dish }) {
             <TouchableOpacity
               className="p-1 rounded-full"
               style={{ backgroundColor: themeColors.bgColor(1) }}
+              onPress={handleDecrease}
+              disabled={!basketItems.length}
             >
               <Icon.Minus
                 strokeWidth={2}
@@ -30,10 +48,11 @@ export default function DishRow({ dish }) {
                 stroke="white"
               />
             </TouchableOpacity>
-            <Text className="px-3"></Text>
+            <Text className="px-3">{basketItems.length}</Text>
 
             <TouchableOpacity
               className="p-1 rounded-full"
+              onPress={handleIncrease}
               style={{ backgroundColor: themeColors.bgColor(1) }}
             >
               <Icon.Plus
